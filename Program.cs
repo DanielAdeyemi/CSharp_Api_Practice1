b3e438bc2b96c65235c3e06fc5b34abaf49aa318
DanielAdeyemi <adeyemidany@gmail.com>
2021-03-28 17:11:50 -0700

    add api

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApiCall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
